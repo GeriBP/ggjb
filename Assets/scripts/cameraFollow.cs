9cfb1c44970f9b2b39a07b40e1f0ff882c42cf02
GeriBP <gbelenguer@gmail.com>
2017-01-20 19:59:52 +0100

    no message

---
+++ Assets/scripts/cameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+    public GameObject target;
+    public float smooth;
+    private float cameraZ;
+    // Use this for initialization
+    void Start () {
+        cameraZ = transform.position.z;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, cameraZ), smooth);
+	}
+}
---
+++ Assets/scripts/playerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+    public float speed, smooth;
+    private Rigidbody2D myRb;
+	// Use this for initialization
+	void Start () {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) //UP
+        {
+            myRb.AddForce(Vector2.up * speed, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.S)) //DOWN
+        {
+            myRb.AddForce(Vector2.down * speed, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.A)) //LEFT
+        {
+            myRb.AddForce(Vector2.left * speed, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.D)) //RIGHT
+        {
+            myRb.AddForce(Vector2.right * speed, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.Space)) //ShockWave
+        {
+            
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 v = myRb.velocity;
+        float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        if (myRb.velocity.magnitude > 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), smooth);
+        }
+    }
+}
