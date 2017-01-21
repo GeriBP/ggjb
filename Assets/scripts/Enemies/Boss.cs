a50b17925c92b518afce284042f9ad7e0b3f7711
Rory Dungan <rorydungan@gmail.com>
2017-01-21 13:58:00 +0100

    Continued working on boss

--- Assets/scripts/Enemies/Boss.cs
+++ Assets/scripts/Enemies/Boss.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 /// <summary>
 /// The final boss of the game
@@ -7,8 +8,34 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Boss : MonoBehaviour, IEnemy
 {
+    [SerializeField]
+    private float startingHP = 1000f;
+
+    /// <summary>
+    /// Can't take damage while the shield is active.
+    /// </summary>
+    private bool shieldActive = false;
+
+    /// <summary>
+    /// Our current hit point. At or below 0 will result in death.
+    /// </summary>
+    private float currentHP;
+
+    private new Rigidbody2D rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        Assert.IsNotNull(rigidbody);
+    }
+
+    void Start()
+    {
+        currentHP = startingHP;
+    }
+
     public void TakeHit(float damage)
     {
-        throw new NotImplementedException();
+        currentHP -= damage;
     }
 }
