8fd99e6da78a052257061619d9ef6cbd83e603f9
GeriBP <gbelenguer@gmail.com>
2017-01-22 00:19:41 +0100

    final wave layout

---
+++ Assets/scripts/destroyIn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyIn : MonoBehaviour {
+    public float time;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, time);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
