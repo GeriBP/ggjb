7dc8a28a76750fcdcd08b3909f7b83258791de64
Rory Dungan <rorydungan@gmail.com>
2017-01-22 12:32:42 +0100

    🔈 Set up gun sound for player 🔫

--- Assets/scripts/playerMove.cs
+++ Assets/scripts/playerMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using FMODUnity;
 using UnityEngine.Assertions;
 
+
+[RequireComponent(typeof(StudioEventEmitter))]
 public class playerMove : MonoBehaviour {
     public float speed, smooth, pickUpPoints, riskPoints, speedPoints, maxScale, speedThreshhold, smoothWave;
     public cameraShake cShake;
@@ -15,7 +17,8 @@ public class playerMove : MonoBehaviour {
     private bool deathBool = false;
     public bool buttonBool = false;
 
-    private StudioEventEmitter soundEmitter;
+    private StudioEventEmitter engineSoundEmitter;
+    private StudioEventEmitter gunSoundEmitter;
 
 	// Use this for initialization
 	void Awake() 
@@ -23,8 +26,10 @@ public class playerMove : MonoBehaviour {
         myRb = GetComponent<Rigidbody2D>();
         Assert.IsNotNull(myRb);
 
-        soundEmitter = GetComponent<StudioEventEmitter>();
-        Assert.IsNotNull(soundEmitter);
+        var soundEmitters = GetComponents<StudioEventEmitter>();
+		Assert.IsTrue(soundEmitters.Length == 2, "Player requires two sound emitters");
+        engineSoundEmitter = soundEmitters[0];
+        gunSoundEmitter = soundEmitters[1];
     }
 	
 	// Update is called once per frame
@@ -57,21 +62,21 @@ public class playerMove : MonoBehaviour {
         }
         if (Input.GetKeyUp(KeyCode.Space) && waveAvailable && !deathBool) //ShockWave
         {
-            StartCoroutine("waveCooldown");
+            StartCoroutine(waveCooldown());
             float meter = GM.waveBar.value;
             if(meter >= 100.0f)
             {
                 //SPECIAL WAVE
                 float proportion = meter / 100.0f;
                 cShake.Shake(proportion);
-                StartCoroutine("shootWave", proportion);
+                StartCoroutine(shootWave(proportion));
                 GM.waveBar.value = 0;
             }
             else if(meter >= 1.0f) //10.0f is the minumum waveMeter you can spend
             {
                 float proportion = meter / 100.0f;
                 cShake.Shake(proportion);
-                StartCoroutine("shootWave", proportion);
+                StartCoroutine(shootWave(proportion));
                 GM.waveBar.value = 0;
             }
         }
@@ -93,7 +98,7 @@ public class playerMove : MonoBehaviour {
 
     public void TakeHit()
     {
-        if(!deathBool) StartCoroutine("death");
+        if(!deathBool) StartCoroutine(death());
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -116,6 +121,8 @@ public class playerMove : MonoBehaviour {
 
     IEnumerator shootWave(float proportion) //16.6f proportioon is max
     {
+        gunSoundEmitter.Play();
+
         var w = Instantiate(wave, transform.position, Quaternion.identity);
         w.Damage = proportion;
         for (int i = 0; i < 50; ++i)
@@ -138,7 +145,7 @@ public class playerMove : MonoBehaviour {
         gameObject.GetComponent<TrailRenderer>().enabled = false;
         if(!deathBool) Instantiate(explosion, transform.position, Quaternion.identity);
         deathBool = true;
-        StartCoroutine("screenDeath");
+        StartCoroutine(screenDeath());
         yield return null;
     }
     IEnumerator screenDeath()
