ea97e3d2dea16a8e75a1c4125e198310f7cbfad6
GeriBP <gbelenguer@gmail.com>
2017-01-22 11:46:26 +0100

    loop complete

--- Assets/scripts/cameraFollow.cs
+++ Assets/scripts/cameraFollow.cs
@@ -14,20 +14,26 @@ public class cameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (pM.myRb.velocity.magnitude > 2.0f)
-        {
-            transform.position = Vector3.Lerp(transform.position, new Vector3(0.0f, 0.0f, cameraZ), smooth);
-            Camera.main.fov = Mathf.Lerp(Camera.main.fov, 88.0f, smoothOut);
+        if (!gameManager.bossDead) {
+            if (pM.myRb.velocity.magnitude > 2.0f)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(0.0f, 0.0f, cameraZ), smooth);
+                Camera.main.fov = Mathf.Lerp(Camera.main.fov, 88.0f, smoothOut);
+            }
+            else
+            {
+                Camera.main.fov = Mathf.Lerp(Camera.main.fov, 55.0f, smoothIn);
+                transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, cameraZ), smooth);
+                if (transform.position.x > 4.0f) transform.position = new Vector3(4.0f, transform.position.y, transform.position.z);
+                else if (transform.position.x < -3.8f) transform.position = new Vector3(-3.8f, transform.position.y, transform.position.z);
+                if (transform.position.y > 3.85f) transform.position = new Vector3(transform.position.x, 3.85f, transform.position.z);
+                else if (transform.position.y < -3.67f) transform.position = new Vector3(transform.position.x, -3.67f, transform.position.z);
+            }
         }
         else
         {
-            Camera.main.fov = Mathf.Lerp(Camera.main.fov, 55.0f, smoothIn);
-            transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, cameraZ), smooth);
-            if (transform.position.x > 4.0f) transform.position = new Vector3(4.0f, transform.position.y, transform.position.z);
-            else if (transform.position.x < -3.8f) transform.position = new Vector3(-3.8f, transform.position.y, transform.position.z);
-            if (transform.position.y > 3.85f) transform.position = new Vector3(transform.position.x, 3.85f, transform.position.z);
-            else if (transform.position.y < -3.67f) transform.position = new Vector3(transform.position.x, -3.67f, transform.position.z);
+            transform.position =new Vector3(0.0f, 0.0f, cameraZ);
+            Camera.main.fov = 88.0f;
         }
-
     }
 }
